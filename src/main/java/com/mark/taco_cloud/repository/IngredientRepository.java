7b69790139077fe061c46676af97ac92188b7a2d
Марк Краснокуцкий <mark.krasnokutsky@osinit.com>
2025-05-23 02:34:45 +0500

    day10: Освоение Spring Data R2DBC на базе данных PostgreSQL

--- src/main/java/com/mark/taco_cloud/repository/IngredientRepository.java
+++ src/main/java/com/mark/taco_cloud/repository/IngredientRepository.java
@@ -2,8 +2,12 @@ package com.mark.taco_cloud.repository;
 
 import com.mark.taco_cloud.domain.dto.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 
-public interface IngredientRepository extends JpaRepository<Ingredient, String> {
+public interface IngredientRepository extends ReactiveCrudRepository<Ingredient, String> {
+
+    Mono<Ingredient> findBySlug(String slug);
 
 }
--- src/main/java/com/mark/taco_cloud/repository/OrderRepository.java
+++ src/main/java/com/mark/taco_cloud/repository/OrderRepository.java
@@ -2,7 +2,10 @@ package com.mark.taco_cloud.repository;
 
 import com.mark.taco_cloud.domain.dto.TacoOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface OrderRepository extends JpaRepository<TacoOrder, Long> {
+public interface OrderRepository extends ReactiveCrudRepository<TacoOrder, Long> {
+
+
 
 }
